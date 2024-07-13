29d517c35b4cada90a064d66844888749d608518
Muskanliaquat7 <muskanliaquat206@gmail.com>
2024-07-13 13:35:52 -0700

    Practice Set  Javascript Chapter-1

--- script.js
+++ script.js
@@ -37,4 +37,47 @@ const item ={
 
 console.log(item["city"])  //New York
 
+// Chapter-1 Practice Set
 
+// Q1. Create a Variable Type string and try to add a Number to it.
+
+let h = "hello";
+let i = "7";
+
+console.log(a+b) //hello6
+
+//When you add any thing to string it will concatenate!
+
+// Q2. use type of operator to find the datatype of string in the last question
+
+console.log(typeof h+i)  //string
+
+// Q3. Create a const object in javascript Can you hold it to change a number later
+
+const asset ={
+    fullName:"justin",
+    isPrinciple: "false",
+    marks:"56"
+    
+}
+
+asset['fullName'] = 'harry' 
+            // OR
+            asset.marks = '78'; 
+
+//you can change the properties of object in const but you cannot add new properties in it 
+
+// Write a javascript program to create a  word-meaning dictionary of 5 words
+
+// Creating the word-meaning dictionary
+const dictionary = {
+    "serendipity": "the occurrence and development of events by chance in a happy or beneficial way",
+    "ephemeral": "lasting for a very short time",
+    "quintessential": "representing the most perfect or typical example of a quality or class",
+    "elucidate": "make (something) clear; explain",
+    "mellifluous": "sweet or musical; pleasant to hear"
+};
+
+console.log(dictionary)  //it will output the whole properties presented in object
+        //   OR
+console.log(dictionary.elucidate)  //it will output the property written above dot in console.log which is elucidate 
