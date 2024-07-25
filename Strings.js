d125499e20017f530c24e5477908ce39092aa8fd
Muskanliaquat7 <muskanliaquat206@gmail.com>
2024-07-25 17:18:22 +0500

    Strings in Javascript!

---
+++ Strings.js
@@ -0,0 +1,47 @@
+// String is a data type of javascript
+
+// strings are created by enclosing text in either double quotes (" "), single quotes (' '), or backticks (` `).
+
+// Creating String:
+
+/* let doubleString = "this is a string";
+let singleString = 'this is a string';
+let templateLeteralString = `this is a string`;
+
+console.log(doubleString);
+console.log(singleString);
+console.log(templateLeteralString); */
+
+//======= Strings Methods =======\\
+
+
+/* String Length */
+
+// (To find out how many characters are in a string, use the .length property):
+
+/*
+ let example = "Hello, World!";
+console.log(example.length); //13
+ */
+
+
+/* Concatenating Strings */
+
+// You can join two or more strings together using the + operator or the concat() method:
+
+/*
+ let firstName = "Muskan"
+let surName = "Liaquat"
+let fullName = firstName + surName;
+let fullName2 = firstName.concat(surName);
+
+console.log(fullName);
+console.log(fullName2); */
+
+
+/* Template Literals */
+// template leterals is also used to add string variables like we do in concatenation but it is the updated method.
+
+/* let name = "Bob";
+let greeting = `Hello, ${name}!`; // "Hello, Bob!"
+ */
