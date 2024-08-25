8717759a7f9d452ee49c2902ade50948838fbd1d
Muskan Liaquat <muskanliaquat206@gmail.com>
2024-08-26 00:30:43 +0500

    Objects in js

--- 02_Basics/01_Array_in_js.js
+++ 02_Basics/01_Array_in_js.js
@@ -1,6 +1,6 @@
-let myToys = ["Doll" , "Poppet" , "Car" , "Puzzle"]
+/* let myToys = ["Doll" , "Poppet" , "Car" , "Puzzle"]
 
 // if i want to get any toy from the Array myToys 
 let myToy = myToys[1]
-console.log(myToy);
+console.log(myToy); */
 
