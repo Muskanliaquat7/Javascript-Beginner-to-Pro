17ede0b089b93a3cad8fed4e635288e5a83a7d88
Muskan Liaquat <muskanliaquat206@gmail.com>
2024-11-26 23:51:14 +0500

    Methods in Class

--- 02_Basics/05_classes_and_objects.js
+++ 02_Basics/05_classes_and_objects.js
@@ -13,7 +13,7 @@ console.log(Car.color); //print just color */
 // class creates a blue print used to create objects.
 // we create class and create objects from it.
 
-class car  {
+/* class car  {
     constructor(brand, color, speed) {
 
         this.brand = brand;
@@ -21,7 +21,7 @@ class car  {
         this.speed = speed;
     
     }
-}
+} */
 
 // constructor is a special type of function that sets properties in it.
 
@@ -37,3 +37,29 @@ console.log(car2); */
 
 
 // Methods in Class
+// If you want to do something with a car, like drive or stop, you use methods.
+
+class Car {
+    constructor(brand, color, speed) {
+      this.brand = brand;
+      this.color = color;
+      this.speed = speed;
+    }
+
+    drive() {
+        console.log(` ${this.brand} is driving at ${this.speed} km/hr. `);
+        
+    }
+
+    stop(){
+        console.log(`${this.brand} has stopped.`);
+        
+    }
+
+}
+
+let car3 = new Car ("haval", "Black", 80);
+
+car3.drive(); // haval is driving at 80 km/hr.
+car3.stop(); //haval has stopped.
+
