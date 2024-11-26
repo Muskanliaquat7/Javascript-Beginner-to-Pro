107001da66511fba2e6d7330d5164cdb3958a982
Muskan Liaquat <muskanliaquat206@gmail.com>
2024-11-26 23:43:45 +0500

    Classes in javascript

--- 02_Basics/05_classes_and_objects.js
+++ 02_Basics/05_classes_and_objects.js
@@ -8,3 +8,20 @@
 console.log(Car); //print whole object
 console.log(Car.brand); //print just brand
 console.log(Car.color); //print just color */
+
+// JavaScript Class Example
+// class creates a blue print used to create objects.
+// we create class and create objects from it.
+
+class car  {
+    constructor(brand, color, speed) {
+
+        this.brand = brand;
+        this.color = color;
+        this.speed = speed;
+    
+    }
+}
+
+// constructor is a special type of function that sets properties in it.
+
