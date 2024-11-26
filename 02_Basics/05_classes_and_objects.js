c646868fd25ff1e04f2ee5cd3e080d72af544f87
Muskan Liaquat <muskanliaquat206@gmail.com>
2024-11-26 23:42:37 +0500

    javascript Objects

---
+++ 02_Basics/05_classes_and_objects.js
@@ -0,0 +1,10 @@
+// JavaScript Object Example
+/* let Car = {
+    brand: "mercedes",
+    color: "white",
+    speed: "200"
+};
+
+console.log(Car); //print whole object
+console.log(Car.brand); //print just brand
+console.log(Car.color); //print just color */
