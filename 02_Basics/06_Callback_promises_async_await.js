2f88cd739c008246c7e20a6dbf8fbc5ede2d4bad
Muskan Liaquat <muskanliaquat206@gmail.com>
2024-11-28 23:36:35 +0500

    .

--- 02_Basics/06_Callback_promises_async_await.js
+++ 02_Basics/06_Callback_promises_async_await.js
@@ -1,8 +1,15 @@
-// Callback
-
-function bakeCake(){
-  console.log("cake is in oven");
-
-  setTimeout( , 3000);
-  
+function khanaBanao(callback) {
+  console.log("Khana bana raha hoon...");
+  setTimeout(() => {
+      console.log("Khana tayar hai!");
+      callback(); // Callback function ko call kiya ja raha hai
+  }, 2000);
 }
+
+function khanaKhao() {
+  console.log("Khana khane laga hoon...");
+}
+
+// Call karte hain
+khanaBanao(khanaKhao);
+
