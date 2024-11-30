2af45894bbc919172e47659b46e599dca1975ffb
Muskan Liaquat <muskanliaquat206@gmail.com>
2024-11-30 12:44:24 +0500

    examples

--- 02_Basics/06_Callback_promises_async_await.js
+++ 02_Basics/06_Callback_promises_async_await.js
@@ -1,4 +1,4 @@
-function khanaBanao(callback) {
+/* function khanaBanao(callback) {
   console.log("Khana bana raha hoon...");
   setTimeout(() => {
       console.log("Khana tayar hai!");
@@ -11,10 +11,90 @@ function khanaKhao() {
 }
 
 // Call karte hain
-khanaBanao(khanaKhao);
+khanaBanao(khanaKhao); */
 
 // output:
 // Khana bana raha hoon...
 // (2 second baad)
 // Khana tayar hai!
 // Khana khane laga hoon...
+
+// Example #2
+/* 
+//Greeting Based on Time
+function greetUser(callback) {
+  console.log("Let's greet user");
+  callback()
+  
+}
+
+function morningGreeting() {
+  console.log("Good Morning! Let's wakeup and get success.");
+  
+}
+
+function eveningGreeting() {
+  console.log("Good Evening! Let's have  a tea time.");
+  
+}
+
+
+greetUser(morningGreeting);
+
+greetUser(eveningGreeting);
+ */
+
+// Example #3
+// 3. Math Operations with Callbacks
+/* 
+function calculate(num1, num2, operation){
+  console.log("Let's start calculating...");
+  const result = operation(num1, num2); //calling callback
+  console.log(`Result: ${result}`);
+  
+}
+
+function add(a, b) {
+  return a + b;
+  
+}
+
+function subtract(a, b) {
+  return a - b;
+  
+}
+
+function multiply(a, b) {
+  return a * b;
+  
+}
+
+function divide(a, b) {
+  return a / b;
+  
+}
+
+// Addition
+calculate(5, 10, add);
+
+// Multiplication
+calculate(5, 10, multiply); */
+
+// Example #4
+// 3. Timer ke Saath Task
+/* 
+function timer( delay, callback){
+  console.log(`Timer set for ${delay / 1000} seconds...`);
+  setTimeout(() => {
+    console.log("timer complete!");
+    callback();
+  }, delay);
+  
+}
+
+function showMessage() {
+  console.log("This message is showing after the timer.");
+  
+}
+
+timer ( 4000, showMessage); */
